f62b6454ff21b4b7f2133ff0b45198e129e48704
NottSr <leonardo.br4@hotmail.com>
2022-05-19 12:31:29 -0500

    Alpha 0.9 - 4th version fixed

--- op_swap.c
+++ op_swap.c
@@ -14,7 +14,7 @@ void swap(stack_t **stack, unsigned int line_num)
 
 	if (len < 2)
 	{
-		fprintf(stderr, "L%d: can't swap, stack too\n", line_num);
+		fprintf(stderr, "L%d: can't swap, stack too short\n", line_num);
 		exit(EXIT_FAILURE);
 	}
 
