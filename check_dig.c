1c0179b9acf3096ece19aa7286b166fb5c3eaae7
NottSr <leonardo.br4@hotmail.com>
2022-05-19 11:17:40 -0500

    Alpha 0.5 - First version fixed

--- check_dig.c
+++ check_dig.c
@@ -1,12 +1,12 @@
 #include "monty.h"
 /**
  * check_dig - function that checks if a str is an int
- * @param: Parameter to check 
- * Return: 0 if success -1 if error 
+ * @param: Parameter to check
+ * Return: 0 if success -1 if error
  */
 int check_dig(char *param)
 {
-    while (*param != '\0')
+	while (*param != '\0')
 	{
 		if (param[0] == 45)
 		{
@@ -19,5 +19,5 @@ int check_dig(char *param)
 
 		++param;
 	}
-    return (0);
+	return (0);
 }
